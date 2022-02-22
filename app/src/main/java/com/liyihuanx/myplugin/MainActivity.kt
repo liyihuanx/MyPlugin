7963fccc38e199e6f3ab76c72ca5e31aa1bd6f1a
liyihuanx <982822040@qq.com>
2022-02-22 23:17:37 +0800

    debug

--- app/src/main/java/com/liyihuanx/myplugin/MainActivity.kt
+++ app/src/main/java/com/liyihuanx/myplugin/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.liyihuanx.myplugin
 
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dalvik.system.DexClassLoader
@@ -14,6 +16,37 @@ class MainActivity : AppCompatActivity() {
 //            loadPluginClass2()
 
             ErrorTest.pluginFun()
+
+            val intent = Intent()
+            intent.component = ComponentName(
+                "com.liyihuanx.plugin",
+                "com.liyihuanx.plugin.MainActivity"
+            )
+            startActivity(intent)
+            // 启动Activity时到进入ASM的流程
+            // activity
+            //   -> Instrumentation#execStartActivity
+            //   -> ActivityTaskManager#IActivityTaskManagerSingleton = (AMS)
+            //   -> AMS#startActivity
+            //   -> ActivityTaskManagerService#startActivityAsUser
+            //   -> ActivityStarter#excute 拿到result
+
+            // Instrumentation#checkStartActivityResult
+
+            // 从ASM检验完回来，继续启动Activity的流程
+            // ActivityStackSupervisor#realStartActivityLocked ->realStartActivityLocked()
+            //      -> mService.getLifecycleManager().scheduleTransaction(clientTransaction)
+            //      -> ClientLifecycleManager.scheduleTransaction
+            //      -> ClientTransaction.schedule()
+            //      -> mClient.scheduleTransaction(this) --> IApplicationThread
+
+            // 回到ActivityThread中的ApplicationThread
+            // -> ActivityThread.this.scheduleTransaction(transaction)
+            // -> ClientTransactionHandler.scheduleTransaction
+            // -> sendMessage(ActivityThread.H.EXECUTE_TRANSACTION, transaction);
+            // -> mH.handleMessage()
+            // 怎么回的intent
+
         }
 
 
@@ -22,14 +55,15 @@ class MainActivity : AppCompatActivity() {
     /**
      * 加载插件方法类
      */
-    private fun loadPluginClass(){
-        val pathClassloader = PathClassLoader("/data/data/com.liyihuanx.myplugin/fix1.dex", classLoader)
+    private fun loadPluginClass() {
+        val pathClassloader =
+            PathClassLoader("/data/data/com.liyihuanx.myplugin/fix1.dex", classLoader)
         val clazz = pathClassloader.loadClass("com.liyihuanx.plugin.ErrorTest")
         val method = clazz.getMethod("pluginFun")
         method.invoke(null)
     }
 
-    private fun loadPluginClass2(){
+    private fun loadPluginClass2() {
         val clazz = Class.forName("com.liyihuanx.plugin.ErrorTest")
         val method = clazz.getMethod("pluginFun")
         method.invoke(null)
--- app/src/main/java/com/liyihuanx/myplugin/ProxyActivity.kt
+++ app/src/main/java/com/liyihuanx/myplugin/ProxyActivity.kt
@@ -1,13 +1,14 @@
-package com.liyihuanx.plugin
+package com.liyihuanx.myplugin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class ProxyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("QWER", "启动插件的Activity")
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_proxy)
+        Log.d("QWER", "启动代理的Activity")
+
     }
 }
